a601e4535745c8caeb470f0f00f4a1efe79f517a
shakiz <activities335@gmail.com>
2020-12-04 00:25:50 +0600

    all activities

---
+++ app/src/main/java/com/client/riseup_labs_assessment/activities/ContentDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.client.riseup_labs_assessment.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.client.riseup_labs_assessment.R;
+
+public class ContentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_details);
+    }
+}
---
+++ app/src/main/java/com/client/riseup_labs_assessment/models/User.java
@@ -0,0 +1,4 @@
+package com.client.riseup_labs_assessment.models;
+
+public class User {
+}
